a520a4cb7bdb2a84c4185bd05c113f5f44144aa1
Tristan Cacqueray <tdecacqu@redhat.com>
2024-01-29 15:15:43 -0500

    Add initial org-external-push command

--- org-external-test.el
+++ org-external-test.el
@@ -5,13 +5,140 @@
 ;;; Code:
 
 (require 'org-external)
+(require 's)
 
-(ert-deftest test-property ()
+(ert-deftest oe/test/parse-external-cat ()
   (should
-   (string=
-    (with-temp-buffer
-      (org-mode)
-      (insert "* new entry")
-      (goto-char (point-min))
-      (oe/has-external (org-element-at-point)))
-    nil)))
+   (equal '(gitlab (:host "gitlab.com" :proj "sf-op")) (oe/parse-external-cat "gitlab.com:sf-op"))
+   )
+  )
+
+(setq oe/example (s-join "\n" (list
+                               "* my goals"
+                               ":PROPERTIES:"
+                               ":CATEGORY_EXTERNAL: jira:localhost:MY_PROJ"
+                               ":END:"
+                               "blah"
+                               "** a goal"
+                               ":PROPERTIES:"
+                               ":END:"
+                               "Goal description:"
+                               ""
+                               "- Do x"
+                               "- Do y"
+                               "*** DONE a task"
+                               "Definition of done: merge X"
+                               )))
+
+(ert-deftest oe/test/push-story ()
+  (with-temp-buffer
+    (org-mode)
+    (insert oe/example)
+    (goto-char (point-min))
+    (search-forward "a goal")
+    ;; Test the calculation
+    (should
+     (equal (oe/push (org-element-at-point))
+            (cons '(jira (:host "localhost" :token nil :proj "MY_PROJ"))
+                  '(create (:summary "a goal" :description "Goal description:\n\n- Do x\n- Do y" :parent nil))
+                  )))
+    ;; The action should create the story
+    (cl-letf (((symbol-function 'plz)
+               (lambda (verb url &rest e)
+                 (should (equal 'post verb))
+                 (should (string= "https://localhost/rest/api/2/issue/" url))
+                 "{\"key\":\"PROJ-1\"}")))
+      (org-external-push))
+    (should
+     (string= "PROJ-1"
+              (org-element-property :EXTERNAL_REF (org-element-at-point))))
+
+    ;; The action should update the story
+    (cl-letf (((symbol-function 'plz)
+               (lambda (verb url &rest e)
+                 (should (equal 'put verb))
+                 (should (string= "https://localhost/rest/api/2/issue/PROJ-1" url))
+                 nil)))
+      (org-external-push))
+    )
+  )
+
+(ert-deftest oe/test/push-task ()
+  (with-temp-buffer
+    (org-mode)
+    (insert oe/example)
+    (goto-char (point-min))
+    ;; Create goal
+    (search-forward "a goal")
+    (org-set-property "EXTERNAL_REF" "PROJ-42")
+    (search-forward "a task")
+    ;; Test the calculation
+    (should
+     (equal (oe/push (org-element-at-point))
+            (cons '(jira (:host "localhost" :token nil :proj "MY_PROJ"))
+                  '(create (:summary "a task" :description "Definition of done: merge X" :parent "PROJ-42"))
+                  )))
+    ;; The action should create the story
+    (cl-letf (((symbol-function 'plz)
+               (lambda (verb url &rest e)
+                 (should (equal 'post verb))
+                 (should (string= "https://localhost/rest/api/2/issue/" url))
+                 "{\"key\":\"PROJ-1\"}")))
+      (org-external-push))
+    (should
+     (string= "PROJ-1"
+              (org-element-property :EXTERNAL_REF (org-element-at-point))))
+
+    ;; The action should update the story
+    (cl-letf (((symbol-function 'plz)
+               (lambda (verb url &rest e)
+                 (should (equal 'put verb))
+                 (should (string= "https://localhost/rest/api/2/issue/PROJ-1" url))
+                 nil)))
+      (org-external-push))
+    )
+  )
+
+
+(ert-deftest oe/test/jira-parse-id ()
+  (let ((resp (s-join "\n" (list
+                            "{"
+                            "\"id\":\"39000\","
+                            "\"key\":\"TEST-101\","
+                            "\"self\":\"http://localhost:8080/rest/api/2/issue/39000\""
+                            "}"
+                            ))))
+    (should
+     (string= "TEST-101" (oe/jira-parse-id resp))))
+  )
+
+(ert-deftest oe/test/jira-payload ()
+  (let* ((base (list
+                "\"project\":{\"key\":\"TEST\"},"
+                "\"summary\":\"REST ye merry gentlemen.\","
+                "\"description\":\"Creating\","
+                "\"issuetype\":{\"name\":\"Story\"}"
+                ))
+         (task (list
+                "\"project\":{\"key\":\"TEST\"},"
+                "\"summary\":\"REST ye merry gentlemen.\","
+                "\"description\":\"Creating\","
+                "\"issuetype\":{\"name\":\"Sub-Task\"},"
+                "\"parent\":{\"key\":\"TEST-42\"}"
+                ))
+         (mk-resp (lambda (body) (format "{\"fields\":{%s}}" (s-join "" body)))))
+    (should
+     (string= (funcall mk-resp base)
+              (oe/jira-create-payload "TEST"
+                                      (list :summary "REST ye merry gentlemen." :description "Creating"))))
+    (should
+     (string= (funcall mk-resp task)
+              (oe/jira-create-payload "TEST"
+                                      (list :summary "REST ye merry gentlemen." :description "Creating" :parent "TEST-42"))))
+    )
+  )
+
+;; (ert "oe/test/**")
+
+(provide 'org-external-test)
+;;; org-external-test.el ends here
--- org-external.el
+++ org-external.el
@@ -24,7 +24,7 @@
 ;; Keywords: org gitlab github jira
 ;; URL: https://github.com/TristanCacqueray/emacs-toolbox
 ;; License: GNU General Public License >= 3
-;; Package-Requires: ((emacs "29") (s "1.2.0"))
+;; Package-Requires: ((emacs "29") (s "1.2.0") (plz "0.7.2"))
 
 ;;; Commentary:
 
@@ -54,12 +54,122 @@
 ;;; Code:
 
 (require 'org)
+(require 's)
+(require 'plz)
+
+(defun oe/parse-external-cat (category-external)
+  "Parse the CATEGORY-EXTERNAL property and return '(type info)."
+  (pcase (s-split ":" category-external)
+    (`("jira" ,host ,proj)
+     (let ((token (auth-info-password (car (auth-source-search :host host :max 1)))))
+       `(jira (:host ,host :token ,token :proj ,proj))))
+    (`("gitlab.com" ,proj) `(gitlab (:host "gitlab.com" :proj ,proj)))
+    (_ (error "Unknown CATEGORY_EXTERNAL property: %s" category-external))))
 
 (defun oe/has-external (item)
-  "Return '(type id) if ITEM has the right property"
-  (let* ((prop (org-entry-get (plist-get item :org-marker) "EXTERNAL")))
-    (message "got %s prop for %s" prop item)
-))
+  "Return '(type info) if ITEM or its parent has the CATEGORY_EXTERNAL property."
+  (if-let ((ext (org-element-property :CATEGORY_EXTERNAL item)))
+      (oe/parse-external-cat ext)
+    (if-let (parent (org-element-property :parent item))
+        (oe/has-external parent))))
+
+(defun oe/get-description (body)
+  "Parse the org BODY content."
+  (let*  ((props-end (when (s-starts-with? ":PROPERTIES:" body t) (s-index-of ":END:" body t)))
+          (chop-props (if props-end (+ props-end 5) 0))
+          (body-without-props (s-chop-left chop-props body))
+          )
+    (s-trim (car (s-split "^\\*\\*" body-without-props)))))
+
+(defun oe/get-body-contents (item)
+  "Return the contents of the ITEM until the next heading **."
+  (let* ((beg (org-element-property :contents-begin item))
+         (end (org-element-property :contents-end item)))
+    (oe/get-description (buffer-substring-no-properties beg end))))
+
+(defun oe/parse-heading (item parent)
+  "Create the action body for heading ITEM and optional PARENT ref."
+  ;; (message "story: %s" item)
+  (let* ((title (org-element-property :title item))
+         (ref (org-element-property :EXTERNAL_REF item))
+         (desc (oe/get-body-contents item))
+         (body `(:summary ,title :description ,desc :parent ,parent)))
+    (if ref `(update ,ref ,body) `(create ,body))))
+
+(defun oe/parse-task (item)
+  "Create the action body for a sub task at ITEM."
+  (if-let ((parent (org-element-property :EXTERNAL_REF (org-element-property :parent item))))
+      (oe/parse-heading item parent)))
+
+(defun oe/push-comment (item)
+  "Push the given ITEM to the SYSTEM as a comment to the parent heading."
+  (message "comment: %s" item)
+  `(create-comment))
+
+(defun oe/push (item)
+  "Return '(system action) or the ITEM."
+  (if-let ((system (oe/has-external item))
+           (level (org-element-property :level item)))
+      (cons system
+            (cond
+             ((= level 4) (oe/push-comment item))
+             ((= level 3) (oe/parse-task item))
+             ((= level 2) (oe/parse-heading item nil))
+             (t (error "Unknown heading"))))
+    (error "Missing CATEGORY_EXTERNAL or point is not on heading")))
+
+(defun oe/run-push (args)
+  "Perform the action from ARGS."
+  (let* ((system (car args))
+         (action (cdr args))
+         (result (pcase (car system)
+                   ('jira (oe/jira-action (cadr system) action))
+                   (_ (error "Unknown system")))))
+    (when result (org-set-property "EXTERNAL_REF" result))))
+
+(defun oe/jira-parse-id (json-str)
+  "Return the key of JSON-STR jira response."
+  (if-let ((json (json-parse-string json-str)))
+      (gethash "key" json nil)))
+
+(defun oe/jira-create-payload (project body)
+  "Create the JSON-STR jira create payload from action PROJECT and BODY."
+  (let* ((parent (plist-get body :parent))
+         (type (if parent "Sub-Task" "Story"))
+         (base `(project (key ,project)
+                         summary ,(plist-get body :summary)
+                         description ,(plist-get body :description)
+                         issuetype (name ,type)))
+         (fields (if parent (plist-put base 'parent `(key ,parent)) base)))
+    (json-serialize (list 'fields fields))))
+
+(defun oe/jira-update-payload (body)
+  "Create the JSON-STR jira create payload from action BODY."
+  (json-serialize `(fields (summary ,(plist-get body :summary)
+                                    description ,(plist-get body :description)))))
+
+(defun oe/jira-action (system-info action)
+  "Perform the ACTION on jira SYSTEM-INFO."
+  (let* ((host (plist-get system-info :host))
+         (auth (format "Bearer %s" (plist-get system-info :token)))
+         (headers `(("Content-Type" . "application/json") ("Authorization" . ,auth))))
+    (pcase (car action)
+      ('create
+       (let* ((url (format "https://%s/rest/api/2/issue/" host))
+              (body (oe/jira-create-payload (plist-get system-info :project) (cadr action)))
+              (resp (plz 'post url :headers headers :body body :as 'string)))
+         (oe/jira-parse-id resp)))
+      ('update
+       (let* ((url (format "https://%s/rest/api/2/issue/%s" host (cadr action)))
+              (body (oe/jira-update-payload (caddr action))))
+         (plz 'put url :headers headers :body body)))
+      (_ (error "Unknown action %s" action)))))
+
+;;;###autoload
+(defun org-external-push ()
+  "Push the heading at point to an external system."
+  (interactive)
+  (oe/run-push (oe/push (org-element-at-point))))
 
 (provide 'org-external)
 ;;; org-external.el ends here
